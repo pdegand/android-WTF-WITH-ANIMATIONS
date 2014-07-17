094c8c6bccc3ba1e6e0dd679638860b8d3bc11c3
Pierre Degand <pdegand@octo.com>
2014-07-17 17:36:15 +0200

    Refactor ApiLevel to use Android constants

--- app/src/main/java/com/octo/pde/androidanimations/structure/ApiLevel.java
+++ app/src/main/java/com/octo/pde/androidanimations/structure/ApiLevel.java
@@ -1,22 +1,24 @@
 package com.octo.pde.androidanimations.structure;
 
+import android.os.Build;
+
 import com.octo.pde.androidanimations.R;
 
 /**
  * @author pde
  */
 public enum ApiLevel {
-    API_LEVEL_1(R.color.color_api_level_1, 1),
-    API_LEVEL_11(R.color.color_api_level_11, 11),
-    API_LEVEL_12(R.color.color_api_level_12, 12),
-    API_LEVEL_16(R.color.color_api_level_16, 16),
-    API_LEVEL_19(R.color.color_api_level_19, 19);
+    API_LEVEL_1(R.color.color_api_level_1, Build.VERSION_CODES.BASE),
+    API_LEVEL_11(R.color.color_api_level_11, Build.VERSION_CODES.HONEYCOMB),
+    API_LEVEL_12(R.color.color_api_level_12, Build.VERSION_CODES.HONEYCOMB_MR1),
+    API_LEVEL_16(R.color.color_api_level_16, Build.VERSION_CODES.JELLY_BEAN),
+    API_LEVEL_19(R.color.color_api_level_19, Build.VERSION_CODES.KITKAT);
     private final int colorResource;
-    private final int apiLevel;
+    private final int sdkInt;
 
-    ApiLevel(int colorRessource, int apiLevel) {
+    ApiLevel(int colorRessource, int sdkInt) {
         this.colorResource = colorRessource;
-        this.apiLevel = apiLevel;
+        this.sdkInt = sdkInt;
     }
 
     public int getColorResource() {
@@ -24,6 +26,6 @@ public enum ApiLevel {
     }
 
     public int getSdkInt() {
-        return apiLevel;
+        return sdkInt;
     }
 }
